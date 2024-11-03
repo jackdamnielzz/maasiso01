f9b76eeebc21488c93afd5167a719f364e781a6b
jackdamnielzz <maassure@gmail.com>
2024-11-03 22:44:28 +0100

    contact

--- add-php-to-path.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Adding PHP to system PATH...
-
-REM Add PHP to system PATH
-setx /M PATH "%PATH%;D:\Downloads\php-8.3.13-nts-Win32-vs16-x64"
-
-echo PHP has been added to PATH. Please restart your terminal to use PHP.
-pause
